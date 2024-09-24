a22ed38cb20bdfd50d43fef388b5f6634c8276ea
Jack Freeman <jack.william.freeman@gmail.com>
2024-09-24 16:30:27 +0800

    added birthdays and embeds.

---
+++ birthdays.py
@@ -0,0 +1,10 @@
+birthdays = {
+    "jack":{
+        "birthday":"1986-07-04"
+        , "video_url":"https://www.youtube.com/watch?v=htP85AsPVZ4"
+        , "discord_user_id" : 550616372120387585
+    }
+}
+
+def get_birthdays():
+    return birthdays
--- main.py
+++ main.py
@@ -3,35 +3,46 @@ import time
 import discord
 from discord.ext import commands, tasks
 from quotes import get_quote
+from birthdays import get_birthdays
 from datetime import datetime as dt
 import pytz
+from dateutil.relativedelta import relativedelta
 
 
+ENV='dev'
 dctx = None
-token = os.environ['TOKEN']
-#token = os.environ['TEST_TOKEN']
-channel_id= int(os.environ['CHANNEL_ID'])
-#channel_id= int(os.environ['TEST_CHANNEL_ID'])
+bot = None
+if ENV == 'prod':
+  token = os.environ['TOKEN']
+  channel_id= int(os.environ['CHANNEL_ID'])
+else:
+  token = os.environ['TEST_TOKEN']
+  channel_id= int(os.environ['TEST_CHANNEL_ID'])
 
 
 @tasks.loop(hours=1)
 async def declare():
     tz = pytz.timezone("Australia/Perth")
     quote = get_quote()
-    message_text = "+++++THOUGHT FOR THE DAY+++++\r\n\r\n"+"+++++THEME:"+quote["topics"][0].upper()+"+++++\r\n\r\n"+quote["text"]
     now=dt.now(tz)
-    print("checking for proverbinatus time is "+ str(now))
+    d1 = dt(2024,11,1,9,0,0,0,tz)
     if now.hour == 3:
+      embed= discord.Embed(title="++++++++THOUGHT OF THE DAY++++++++", type='rich', color=discord.Color.red())
+      embed.set_author(name="Office of the Inquisition (Ordo Proverbinatus)",icon_url="https://www.belloflostsouls.net/wp-content/uploads/2016/09/Inquisition.jpg")
+      embed.add_field(inline=False, name=f"{quote['text']}", value= '')
+      embed.add_field(inline=False, name=f"+++++++++THEME+++++++++", value= ', '.join(quote['topics']))
+      embed.set_footer(text=f"Go about your day with purpose citizen. The Inquisition maintains a vigilant eye for heresy. The Emperor Protects.")
       print("proverbinatus sent")
-      await dctx.send(message_text)
-      d1 = dt(2022,9,29,9,0,0,0,tz)
+      await dctx.send(embed=embed)
+      d1 = dt(2024,11,1,9,0,0,0,tz)
       length = d1 - now
       if length.days > 1:
-        await dctx.send(f"WARNING: {length.days} DAYS UNTIL SFTC")
+        await dctx.send(f"WARNING: {length.days} DAYS UNTIL SFTC. LOYAL CITIZENS, PREPARE YOURSELF FOR MIRTH")
       if length.days == 1:
         await dctx.send("WARNING: TOMORROW IS SFTC")
       if length.days == 0:
         await dctx.send("WARNING: TODAY IS SFTC, EMPEROR BE WITH US ALL")
+      await handle_birthdays(dctx)
 
 bot = commands.Bot(command_prefix="!40kbot ", case_insensitive=True)
 
@@ -42,8 +53,26 @@ async def on_ready():
     print(f'{bot.user.name} has connected to discord!')
     await bot.change_presence(activity = discord.Activity(
                           type = discord.ActivityType.watching, 
-                          name = 'for Ian storytime updates'))
+                          name = 'steamed hams netflix true crime docos'))
     print("heresy detected. starting proverbinatus")
     declare.start()
+
+async def handle_birthdays(dctx):
+  birthdays = get_birthdays()
+  for person in birthdays:
+      birthday = birthdays.get(person).get("birthday")
+      discord_mention_name = f"<@{birthdays.get(person).get('discord_user_id')}>"
+      birthday_dt = dt.strptime(birthday, "%Y-%m-%d")
+      now = dt.now()
+      if birthday_dt.day == now.day and birthday_dt.month == now.month:
+        years_old = relativedelta(dt.now(), birthday_dt).years
+        embed= discord.Embed(title="++++++++BIRTHDAY HERESY DETECTED!++++++++", type='rich', description="Details of the **Heretic** and their crimes are outlined below:", color=discord.Color.red())
+        embed.set_author(name="Office of the Inquisition (Ordo Birthdayeus)",icon_url="https://www.belloflostsouls.net/wp-content/uploads/2016/09/Inquisition.jpg")
+        embed.add_field(inline=False, name=f"Heretical Offence:", value=f"Growing older and thus seeking to diminish your service To the God Emperor of Mankind with pointless celebration and needless consumption")
+        embed.add_field(inline=False, name=f"Convicted Heretic:", value=f"The vile creature know as **{person}**, they have thus far delivered **{years_old}** years of (previously) impeccable service to our (undying) Lord.")
+        embed.add_field(inline=False, name=f"Punishment for Heresy:", value=f"Chastistment by the **Canticles of Age Heresy Rejection**. Prepare yourself for your sentence!")
+        embed.set_footer(text=f"In Fealty to our (undying) God Emperor and by the Grace of the Golden Throne\r\nI denounce {person} as an age-heretic and hope that they may return to the Grace of our (undying) Lord and serve Him faithfully\r\nMay Imperial Justice account in all balance.\r\nThe Emperor Protects")
+        await dctx.send(embed=embed)
+        await dctx.send(f"Now we will commence the sentence: Singing the glorious hymn: **THE CANTICLES OF AGE HERESY REJECTION** in chastisement of convicted age-heretic: {discord_mention_name}.\r\n\r\n{birthdays.get(person).get('video_url')}")
 
 bot.run(token)
